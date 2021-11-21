7dd315078d26d3f8ef330d7c8a292f7a5f63b24d
Andrey Zhilin <komplayer31@yandex.ru>
2021-11-21 19:52:50 +0900

    Initial Commit

---
+++ TMNT Quiz Code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMNT Quiz Code
+//
+//  Created by Андрей on 21.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
